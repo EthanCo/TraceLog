de5a516e182fa3652bfb99e68bcdd382a9c51cbd
Heiko-HP <fenghenyu@163.com>
2020-02-14 17:09:22 +0800

    L.java类升级

--- logglobal/src/main/java/com/lib/utils/print/L.java
+++ logglobal/src/main/java/com/lib/utils/print/L.java
@@ -2,172 +2,195 @@ package com.lib.utils.print;
 
 import android.content.Context;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.ethanco.tracelog.TraceLog;
+import com.ethanco.tracelog.logs.DefaultLog;
 import com.ethanco.tracelog.logs.DiskLogTrace;
 
+
 /**
- * 全局Log
+ * Log
  */
 public class L {
+    public static boolean isDebug = true; //是否是调试状态
+    private static int minLevel = 0x1;
+    private static int stackIndex = 5;
+
+    public static final int V = 0x1;
+    public static final int D = 0x2;
+    public static final int I = 0x3;
+    public static final int W = 0x4;
+    public static final int E = 0x5;
+    public static final int A = 0x6;
+
     private static TraceLog traceLog;
-
-    public static void init() {
-        TraceLog traceLog = new TraceLog.Builder()
-                .addTrace(new LTrace())
-                .build();
-        L.init(traceLog);
-    }
-
-    public static void init(String defTag) {
-        TraceLog traceLog = new TraceLog.Builder()
-                .addTrace(new LTrace())
-                .setDefaultTag(defTag)
-                .build();
-        L.init(traceLog);
-    }
+    private static boolean autoPrintStatckInfo = false;
 
     /**
-     * 初始化L日志类
+     * 是否自动打印堆栈信息 (Verbose级别)
      *
-     * @param context
-     * @param defTag 默认tag
-     * @param saveToDisk 是否保存到本地
+     * @param autoPrintStatckInfo
      */
-    public static void init(Context context, String defTag, boolean saveToDisk) {
-        TraceLog.Builder builder = new TraceLog.Builder()
-                .addTrace(new LTrace());
-        if (!TextUtils.isEmpty(defTag)) {
-            builder.setDefaultTag(defTag);
-        }
-        if (saveToDisk) {
-            builder.addTrace(new DiskLogTrace(context));
-        }
-        TraceLog traceLog = builder.build();
-        L.init(traceLog);
+    public static void setAutoPrintStatckInfo(boolean autoPrintStatckInfo) {
+        L.autoPrintStatckInfo = autoPrintStatckInfo;
     }
 
     /**
-     * 初始化L日志类，自定义配置
+     * 初始化
      *
+     * @param enable 是否打印日志
+     */
+    public static void init(Context context, boolean enable) {
+        init(context, enable, false);
+    }
+
+    /**
+     * 初始化
+     *
+     * @param enable 是否打印日志
+     */
+    public static void init(Context context, boolean enable, boolean saveToDisk) {
+        isDebug = enable;
+        TraceLog.Builder builder = new TraceLog.Builder()
+                .addTrace(new DefaultLog());
+        if (saveToDisk) {
+            builder.addTrace(new DiskLogTrace(context, 1024 * 1024 * 100,
+                    "Log", "Log", false));
+        }
+        traceLog = builder.build();
+    }
+
+    /**
+     * 初始化
+     *
+     * @param enable
      * @param _traceLog
      */
-    public static void init(TraceLog _traceLog) {
+    public static void init(boolean enable, TraceLog _traceLog) {
+        isDebug = enable;
         traceLog = _traceLog;
     }
 
-    public static void e(String msg) {
-        traceLog.e(msg);
+    /**
+     * 允许打印的最低级别
+     *
+     * @param _level
+     */
+    public static void setMinLevel(int _level) {
+        minLevel = _level;
     }
 
-    public static void e(String msg, Throwable tr) {
-        traceLog.e(tr, msg);
-    }
-
-    public static void w(String msg) {
-        traceLog.w(msg);
-    }
-
-    public static void i(String msg) {
-        traceLog.i(msg);
-    }
-
-    public static void d(String msg) {
-        traceLog.d(msg);
-    }
-
-    public static void v(String msg) {
-        traceLog.v(msg);
-    }
-
-    public static void json(String json) {
-        traceLog.json(json);
-    }
-
-    public static void xml(String xml) {
-        traceLog.xml(xml);
-    }
-
-    public static void v(Object object) {
-        traceLog.v(object);
-    }
-
-    public static void i(Object object) {
-        traceLog.i(object);
-    }
-
-    public static void d(Object object) {
-        traceLog.d(object);
-    }
-
-    public static void w(Object object) {
-        traceLog.w(object);
-    }
-
-    public static void e(Object object) {
-        traceLog.e(object);
-    }
-
-    public static PrinterWrap t(String tag) {
-        return getPrintWrap(traceLog, tag);
-    }
-
-    private static PrinterWrap getPrintWrap(TraceLog traceLog, String tag) {
-        return new PrinterWrap(traceLog.t(tag));
-    }
-
-
-    //---------------------------- 其他(兼容原先) ---------------------------------------
-
-    public static void e(String tag, String msg) {
-        traceLog.t(tag).e(msg);
-    }
-
-    public static void e(String tag, String msg, Throwable tr) {
+    private static void error(String tag, String msg, Throwable tr) {
+        if (checkLog(E, msg)) return;
         traceLog.t(tag).e(tr, msg);
     }
 
-    public static void w(String tag, String msg) {
-        traceLog.t(tag).w(msg);
+    private static void print(int type, String TAG, String msg) {
+        if (checkLog(type, msg)) return;
+        String message = getLogMessage(msg);
+
+        switch (type) {
+            case V:
+                traceLog.t(TAG).v(message);
+                break;
+            case D:
+                traceLog.t(TAG).d(message);
+                break;
+            case I:
+                traceLog.t(TAG).i(message);
+                break;
+            case W:
+                traceLog.t(TAG).w(message);
+                break;
+            case E:
+                traceLog.t(TAG).e(message);
+                break;
+            case A:
+                traceLog.t(TAG).wtf(message);
+                break;
+            default:
+                throw new IllegalArgumentException("not support is type:" + type);
+        }
+        if (autoPrintStatckInfo && type > V) {
+            traceLog.t(TAG).v(new StringBuilder(message).append(getStackInfo()));
+        }
     }
 
-    public static void i(String tag, String msg) {
-        traceLog.t(tag).i(msg);
+    private static String getLogMessage(String msg) {
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        String className = stackTrace[stackIndex].getFileName();
+        String methodName = stackTrace[stackIndex].getMethodName();
+        int lineNumber = stackTrace[stackIndex].getLineNumber();
+        //methodName = methodName.substring(0, 1).toUpperCase() + methodName.substring(1);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[ (").append(className).append(":")
+                .append(lineNumber).append(")#")
+                .append(methodName).append(" ] ")
+                .append(msg);
+
+        return stringBuilder.toString();
     }
 
-    public static void d(String tag, String msg) {
-        traceLog.t(tag).d(msg);
+    private static boolean checkLog(int type, String msg) {
+        if (!isDebug) {
+            return true;
+        }
+        if (type < minLevel) {
+            return true;
+        }
+        if (TextUtils.isEmpty(msg)) {
+            return true;
+        }
+        return false;
     }
 
-    public static void v(String tag, String msg) {
-        traceLog.t(tag).v(msg);
+    public static void e(String TAG, String msg) {
+        print(E, TAG, msg);
     }
 
-    public static void json(String tag, String json) {
-        traceLog.t(tag).json(json);
+    public static void e(String TAG, String msg, Throwable tr) {
+        error(TAG, msg, tr);
     }
 
-    public static void xml(String tag, String xml) {
-        traceLog.t(tag).xml(xml);
+    public static void w(String TAG, String msg) {
+        print(W, TAG, msg);
     }
 
-    public static void v(String tag, Object object) {
-        traceLog.t(tag).v(object);
+    public static void i(String TAG, String msg) {
+        print(I, TAG, msg);
     }
 
-    public static void i(String tag, Object object) {
-        traceLog.t(tag).i(object);
+    public static void d(String TAG, String msg) {
+        print(D, TAG, msg);
     }
 
-    public static void d(String tag, Object object) {
-        traceLog.t(tag).d(object);
+    public static void v(String TAG, String msg) {
+        print(V, TAG, msg);
     }
 
-    public static void w(String tag, Object object) {
-        traceLog.t(tag).w(object);
+    private static final String LINE_FEED = "\n    ";
+    private static final String AT = "at ";
+
+    public static String getStackInfo() {
+        return getStackInfo("");
     }
 
-    public static void e(String tag, Object object) {
-        traceLog.t(tag).e(object);
+    public static String getStackInfo(String prefix) {
+        StringBuilder sb = new StringBuilder();
+        if (!TextUtils.isEmpty(prefix)) {
+            sb.append(prefix);
+        }
+        sb.append(LINE_FEED);
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        for (int i = 4; i < stackTrace.length; i++) {
+            sb.append(AT).append(stackTrace[i].toString()).append(LINE_FEED);
+        }
+        return sb.toString();
+    }
+
+    public static String getStackInfo(Throwable tr) {
+        return Log.getStackTraceString(tr);
     }
 }
